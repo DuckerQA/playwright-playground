213b33ffda99ead954cf35eebb4da17dca2cba75
Ducker <mateuszczerewko@gmail.com>
2024-11-26 08:54:36 +0100

    chore: refactor of product page

--- tests/automationexercise.com/tests/smoke/products.spec.ts
+++ tests/automationexercise.com/tests/smoke/products.spec.ts
@@ -5,7 +5,6 @@ test.describe('Verify Products Page', () => {
     productsPage,
   }) => {
     await productsPage.goToAllProducts();
-    await expect(productsPage.productsTitle).toContainText('All Products');
     await expect(productsPage.productsList).toBeVisible();
     await expect(productsPage.firstProduct).toBeVisible();
   });
@@ -13,10 +12,13 @@ test.describe('Verify Products Page', () => {
   test('should navigate to products page and view all subcategories from the WOMEN category', async ({
     productsPage,
   }) => {
+    const dressSubcategory = 'Dress';
+    const topsSubcategory = 'Tops';
+    const sareeSubcategory = 'Saree';
     await productsPage.goToAllProducts();
     await productsPage.womenCategory.click();
-    await expect(productsPage.womenSubcategory('Dress')).toBeVisible();
-    await expect(productsPage.womenSubcategory('Tops')).toBeVisible();
-    await expect(productsPage.womenSubcategory('Saree')).toBeVisible();
+    await expect(productsPage.womenSubcategory(dressSubcategory)).toBeVisible();
+    await expect(productsPage.womenSubcategory(topsSubcategory)).toBeVisible();
+    await expect(productsPage.womenSubcategory(sareeSubcategory)).toBeVisible();
   });
 });
